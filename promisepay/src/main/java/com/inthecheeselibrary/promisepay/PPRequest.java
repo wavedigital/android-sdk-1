de69d67b77abf04a9cff004c9f800fa89a4d8e34
KevinHakans <kevin.hakans.it@gmail.com>
2016-01-25 07:33:40 +0100

    initial commit

---
+++ promisepay/src/main/java/com/inthecheeselibrary/promisepay/PPRequest.java
@@ -0,0 +1,155 @@
+package kevinhakans.promisepay;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PPRequest {
+	private URL url;
+	private HttpURLConnection con;
+	private OutputStream os;
+
+
+	/**
+	 * Can be instantiated with java.net.URL object, which checked for MalformedURLException by caller, so no need to declare here
+	 *
+	 * @param url
+	 */
+	public PPRequest(URL url){
+		this.url=url;
+	}
+
+	/**
+	 * Can be instantiated with String representation of url, force caller to check for MalformedURLException which can be thrown by URL's constructor
+	 *
+	 * @param url
+	 * @throws MalformedURLException
+	 */
+	public PPRequest(String url)throws MalformedURLException{
+		this.url=new URL(url);
+	}
+
+	/**
+	 * used by all sending methods: send(), sendAndReadString(), sendAndReadJson() - to close open resources
+	 *
+	 * @throws IOException
+	 */
+	private void done()throws IOException{
+		con.disconnect();
+		os.close();
+	}
+
+	/**
+	 * Sending connection and opening an output stream to server by pre-defined instance variable url
+	 * 
+	 * @param isPost boolean - indicates whether this request should be sent in POST method
+	 * @throws IOException - should be checked by caller
+	 * */
+	private void prepareAll(boolean isPost)throws IOException{
+		con = (HttpURLConnection) url.openConnection();
+		if(isPost)con.setRequestMethod("POST");
+		con.setDoOutput(true);
+		con.setDoInput(true);
+        os = con.getOutputStream();
+	}
+
+	/**
+	 * prepare request in GET method
+	 *
+	 * @return
+	 * @throws IOException
+	 */
+	public PPRequest prepare() throws IOException{
+		prepareAll(false);
+		return this;
+	}
+
+	/**
+	 * prepare request in POST method
+	 *
+	 * @return
+	 * @throws IOException
+	 */
+	public PPRequest preparePost() throws IOException{
+		prepareAll(true);
+		return this;
+	}
+
+	/**
+	 * Writes query to open stream to server
+	 *
+	 * @param query
+	 * @return
+	 * @throws IOException
+	 */
+	public PPRequest withData(String query) throws IOException{
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+        writer.write(query);
+        writer.close();
+		return this;
+	}
+
+	/**
+	 *
+	 * @param params
+	 * @return
+	 * @throws IOException
+	 */
+	public PPRequest withData(HashMap<String,String> params) throws IOException{
+		String result="",and = "";
+        for(Map.Entry<String, String> entry : params.entrySet()){
+            result+=and+entry.getKey()+"="+entry.getValue();//concats: key=value (for first param) OR &key=value(second and more)
+            if(and.isEmpty())and="&";//& between params, except first so added after first concatenation
+        }
+        withData(result);
+		return this;
+	}
+
+	/**
+	 * When caller only need to send, and don't need String response from server
+	 *
+	 * @return
+	 * @throws IOException
+	 */
+	public boolean send() throws IOException{
+		boolean status = con.getResponseCode()==HttpURLConnection.HTTP_OK;//Http OK == 200
+		done();
+		return status; //boolean return to indicate whether it successfully sent
+	}
+
+	/**
+	 * Sending request to the server and pass to caller String as it received in response from server
+	 * 
+	 * @return String printed from server's response
+	 * @throws IOException - should be checked by caller
+	 * */
+	public String sendAndReadString() throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String line,response="";
+        while ((line=br.readLine()) != null)response+=line;
+        done();
+		return response;
+	}
+
+	/**
+	 * JSONObject representation of String response from server
+	 *
+	 * @return
+	 * @throws JSONException
+	 * @throws IOException
+	 */
+	public JSONObject sendAndReadJSON() throws JSONException, IOException{
+		return new JSONObject(sendAndReadString());
+	}
+}
---
+++ promisepay/src/main/java/com/inthecheeselibrary/promisepay/PromisePay.java
@@ -0,0 +1,111 @@
+package kevinhakans.promisepay;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.HashMap;
+
+
+/**
+ * Created by Kevin Hakans on 1/14/16.
+ */
+public class PromisePay {
+
+    /**
+     * The instance.
+     */
+    private static PromisePay instance;
+
+    /**
+     * The environment.
+     */
+    private String environment;
+
+    /**
+     * The publicKey.
+     */
+    private String publicKey;
+
+    /**
+     * The apiUrl.
+     */
+    private String apiUrl;
+
+
+    /**
+     * Get the PromisePay Instace
+     *
+     * @return
+     */
+    public static PromisePay getInstance() {
+        if (instance == null) {
+            instance = new PromisePay();
+        }
+        return instance;
+    }
+
+    /**
+     * Configuration method
+     *
+     * @param environment - Environment of package ("prelive" or "prod")
+     * @param publicKey Public key provided by PromisePay.
+     */
+    public void initialize(String  environment, String publicKey) {
+        this.environment = environment;
+        this.publicKey = publicKey;
+        if (environment.equals("dev"))
+            this.apiUrl = "http://api.localhost.local:3000/";
+        else if (environment.equals("prelive"))
+            this.apiUrl = "https://test.api.promisepay.com/";
+        else if (environment.equals("prod"))
+            this.apiUrl = "https://secure.api.promisepay.com/";
+    }
+
+    /**
+     * Create Card Account method
+     *
+     * @param cardToken - A one-time use token that lasts for 20 minutes. Generate using https://promisepay-api.readme.io/docs/generate-a-card-token. It is assigned to a user within the marketplace to authorize the creation of a card account. It is NOT the token for a credit account that is generated, that is the 'Card Account ID'.
+     * @param card - Full card information (eg. { full_name: "Bobby Buyer", number: "4111111111111111", expiry_month: "02", expiry_year: "2018", cvv: "123" })
+     * @param onPromiseRequestListener - Custom function to call back once card creation is a success or failure
+     */
+    public void createCardAccount(String cardToken, PPCard card, OnPromiseRequestListener onPromiseRequestListener) {
+        String url = String.format("%scard_accounts?card_token=%s", this.apiUrl, cardToken);
+        httpRequest(url, card.parameters(), onPromiseRequestListener);
+    }
+
+    /**
+     * Listener Interface
+     */
+    public interface OnPromiseRequestListener {
+        public void onSuccess(JSONObject response);
+        public void onError(Exception e);
+    }
+
+    /**
+     * Post request to PromisePay API Server
+     *
+     * @param url - The url of request.
+     * @param parameters -  The parameters of request.
+     * @param onPromiseRequestListener - The listener of response.
+     */
+    public void httpRequest(String url, HashMap<String, String> parameters, OnPromiseRequestListener onPromiseRequestListener) {
+        try {
+            PPRequest request = new PPRequest(url);
+            try {
+                onPromiseRequestListener.onSuccess(request.preparePost().withData(parameters).sendAndReadJSON());
+            } catch (JSONException e) {
+                onPromiseRequestListener.onError(e);
+                e.printStackTrace();
+            } catch (IOException e) {
+                onPromiseRequestListener.onError(e);
+                e.printStackTrace();
+            }
+
+        } catch (MalformedURLException e) {
+            onPromiseRequestListener.onError(e);
+            e.printStackTrace();
+        }
+    }
+}
